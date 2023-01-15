9ba07617e9da9f5767c4875feb420ee9be5f8551
akrhode <a.rhode@web.de>
2023-01-15 11:04:16 +0100

    Add countdown functions

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # days-countdown-timer
+
+Live countdown timer built with the JS library Day.js. The timer shows:
+
+<p>
+<ul>
+<li>The date until which the countdown runs<li>
+<li>The countdown of days, hours, minutes, and seconds<li>
+<li>The current date (day, month, year) and the current time<li>
+</ul>
--- skript.js
+++ skript.js
@@ -1,4 +1,46 @@
-// Current Time Section
+// Integrate dayjs library
+dayjs.extend(dayjs_plugin_duration);
+
+// Countdown
+function activateCountdown(element, dateString) {
+  const targetDate = dayjs(dateString);
+
+  element.querySelector(
+    ".until--event"
+  ).textContent = `Until ${targetDate.format("D MMMM YYYY")}`;
+
+  setInterval(() => {
+    const now = dayjs();
+    const duration = dayjs.duration(targetDate.diff(now));
+
+    if (duration.asMilliseconds() <= 0) return;
+
+    element.querySelector(".until--numeric--seconds").textContent = duration
+      .seconds()
+      .toString()
+      .padStart(2, "0");
+
+    element.querySelector(".until--numeric--minutes").textContent = duration
+      .minutes()
+      .toString()
+      .padStart(2, "0");
+
+    element.querySelector(".until--numeric--hours").textContent = duration
+      .hours()
+      .toString()
+      .padStart(2, "0");
+
+    element.querySelector(".until--numeric--days").textContent = duration
+      .asDays()
+      .toFixed(0)
+      .toString()
+      .padStart(2, "0");
+  }, 250);
+}
+
+activateCountdown(document.getElementById("myCountdown"), "2024-12-31");
+
+// Display current time
 const timeElement = document.querySelector(".time");
 const dateElement = document.querySelector(".date");
 
@@ -16,7 +58,7 @@ function formatTime(date) {
     .padStart(2, "0")} ${isAm ? "AM" : "PM"}`;
 }
 
-// Current Date Section
+// Display current date
 /**
  *
  * @param {Date} date
